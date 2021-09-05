e6c0ba0fb4606235bfe63105dcfa74da23281b36
AliTinai <ali-009@windowslive.com>
2021-09-05 11:32:16 +0300

    Made the contacts page more distinct from the homepage

--- src/contacts/contacts.js
+++ src/contacts/contacts.js
@@ -2,6 +2,7 @@
 import '../home/navbar.css';
 import '../home/footer.css';
 import backgroundImage from './contacts-page-background.jpg';
+import './contacts.css';
 
 export function loadContactsPage(){
 
@@ -17,26 +18,28 @@ export function loadContactsPage(){
 
 function createMainContent(){
   const mainContent = document.createElement('div');
-  const ourStory = document.createElement('div');
 
-  const restaurantName = document.createElement('h1');
-  restaurantName.textContent = 'Burgers and Steak';
+  const contactList = document.createElement('ul');
+  contactList.setAttribute('id','contacts-list');
+  const contact = [];
 
-  const ourStoryParagraph = document.createElement('p');
-  ourStoryParagraph.textContent = "These are our contacts";
+  //The contents of the contacts array
+  const contactText = [];
+  contactText[0] = `Joe Doe
+joe.doe@burgerandsteaks.com`;
+  contactText[1] = `Eugene H.Krabs
+eugene.krabs@burgersandsteaks.com`;
+  contactText[2] = `Bob Burger
+bob.burger@burgerandsteaks.com`;
 
-  ourStory.setAttribute('id','our-story');
-
-  const lineBreak = document.createElement('br');
-
-  let ourStoryArray = [restaurantName, lineBreak, ourStoryParagraph];
-
-  //Appenidng content to ourStory div
+  //creating an array of contacts
   for(let i = 0; i < 3; i++){
-    ourStory.appendChild(ourStoryArray[i]);
+      contact[i] = document.createElement('li');
+      contact[i].textContent = contactText[i];
+      contactList.appendChild(contact[i]);
   }
-  mainContent.setAttribute('id','main-content');
-  mainContent.appendChild(ourStory);
-  return mainContent;
 
+  mainContent.setAttribute('id','main-content');
+  mainContent.appendChild(contactList);
+  return mainContent;
 }
